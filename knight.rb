7c60c7c585545dec8e1fc2cac1eb708911074125
Brian Heithaus <bheithaus@gmail.com>
2013-03-17 00:25:12 -0700

    finish knights travails

--- knight.rb
+++ knight.rb
@@ -1,5 +1,5 @@
 require 'set'
-require 'tree'
+require './tree'
 
 class Knight
 
@@ -12,15 +12,31 @@ class Knight
 			 [-1,2],
 			 [-2,1]]
 
+	def knight_path(start_pos, end_pos)
+		## make a tree starting from the start_pos
+		root_node = move_tree( TreeNode.new( start_pos ) )
+		## find the shortest path to the end_pos
+		end_node = root_node.bfs( end_pos )
+		array_of_positions = []
+		node_tracer = end_node
+		##follow parent chain to track series of moves
+		until node_tracer.parent.nil?
+			array_of_positions << node_tracer.value
+			node_tracer = node_tracer.parent
+		end
+		array_of_positions << node_tracer.value
+
+		array_of_positions.reverse
+	end
+
+private
+
 	def move_tree(root_node)
-		used_moves = Set.new
+		used_moves = Set.new(root_node.value)
 		newest_nodes = [root_node]
-
-		8.times do |index|
-			next_newest_nodes = []
-
+		6.times do |index|   ## how to handle this in a more extensible way??
+			newest_nodes_in_training = []   ## tracking the new nodes to be
 			newest_nodes.each do |node|
-
 				start = node.value
 				MOVES.each do |move|
 					new_move = [ move[0] + start[0], move[1] + start[1] ]  #x,y
@@ -28,16 +44,14 @@ class Knight
 						used_moves.add( new_move )
 						new_node = TreeNode.new(new_move) 
 						node.add_child( new_node )
-						next_newest_nodes << new_node
+						newest_nodes_in_training << new_node
 					end
 				end
 			end
-			newest_nodes = next_newest_nodes
+			newest_nodes = 	newest_nodes_in_training
 		end
-		[ root_node , used_moves ] 
+		root_node
 	end
-	
-private
 
 	def in_bounds?(move)
 		( move[0] <= 8 && move[0] >= 1 ) && ( move[1] <= 8 && move[1] >= 1 )
@@ -47,10 +61,15 @@ end
 
 ###tester method
 def p
-	root = TreeNode.new( [5,4] )
+
 	dr_knight = Knight.new
-	root , used_moves = dr_knight.move_tree( root )
-	root.print_tree
-	print "you found : #{used_moves.length} moves"
-	nil
+
+	dr_knight.knight_path([8, 1],[1, 1])
+	# root = TreeNode.new( [5,4] )
+	# dr_knight = Knight.new
+	# root , used_moves = dr_knight.move_tree( root )
+	# root.print_tree
+	# print "you found : #{used_moves.length} moves"
+	# nil
+
 end
